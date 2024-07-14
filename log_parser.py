9bfa5c7c89713d120f49f6e35b44e25d4bebca4f
brossignol <benoit-rossignol@hotmail.fr>
2024-07-14 18:10:12 +0200

    init

---
+++ log_parser.py
@@ -0,0 +1,98 @@
+import json
+
+
+def parse_line(line):
+    i = line.find('{')
+    j = line[::-1].find('}')
+    s = line[i:-j + 1]
+    if s.count('{') > s.count('}'):
+        s += '}'  # sometime the last parenthesis is missing
+    return json.JSONDecoder().decode(s)
+
+
+def get_player_map(e):
+    player_map = {p['LoginID']: p['PlayerName'] for p in e["Players"]}
+    player_map[-99] = 'Bye'
+    return player_map
+
+
+def get_standing_at_round(rd, player_map):
+    n = len(rd['Results'])  # n_player
+    n_round = rd.get('Round', rd.get('Number'))
+    df = {'Rank': [''] * n, 'Name': [''] * n, 'Wins': [''] * n, 'Losses': [''] * n}
+
+    for i in range(1, n_round + 1):
+        df[f'R{i}'] = [''] * n
+        df[f'R{i}w'] = [''] * n
+        df[f'R{i}l'] = [''] * n
+
+    for player in rd['Results']:
+        i = player['Rank'] - 1
+        df['Rank'][i] = i + 1
+        df['Name'][i] = player_map[player['LoginID']]
+        df['Wins'][i] = 0
+        df['Losses'][i] = 0
+
+        for opponent in player['OpponentResults']:
+            j = n_round - opponent['Round'] + 1
+            df[f'R{j}'][i] = player_map[opponent['LoginID']]
+            df[f'R{j}w'][i] = opponent['Win']
+            df[f'R{j}l'][i] = opponent['Loss']
+
+            if opponent['Bye'] or opponent['Win'] > opponent['Loss']:
+                df['Wins'][i] += 1
+            else:
+                df['Losses'][i] += 1
+    return df
+
+
+def parse_started_event(log):
+    # load already started event
+    challenges = {}
+    for line in log:
+        if 'PremiereEventSyncData":{' in line:
+            e = parse_line(line)
+            if len(e["Players"]) == 0:
+                continue
+            rounds = e["PremiereEventSyncData"]['TournamentSyncData']['Rounds']
+            player_map = get_player_map(e)
+
+            challenge = {'name': e['Description'] + e['StartDate'][:13], 'player_map': player_map}
+            challenges[e['EventToken']] = challenge
+
+            finished = [rd for rd in rounds if rd['Results']]
+            if len(finished) > 0:
+                challenge['standings'] = get_standing_at_round(finished[-1], player_map)
+            else:
+                challenge['standings'] = {}
+
+    return challenges
+
+
+def update_player_map(log, challenges):
+    # get player map from round start message
+    for line in log:
+        if 'FlsTournamentRoundInfoMessage' in line and 'Matches' in line:
+            e = parse_line(line)
+
+            challenge = challenges.get(e['Token'])
+            if not challenge:
+                continue
+
+            for match in e['Round']['Matches']:
+                for player in match['Players']:
+                    if player['LoginID'] not in challenge['player_map']:
+                        challenge['player_map'][player['LoginID']] = player['PlayerName']
+
+
+def update_standings(log, challenges):
+    # get standings from round results
+    for line in log:
+        if 'FlsTournamentRoundResultMessage' in line and 'Results' in line:
+            e = parse_line(line)
+
+            challenge = challenges.get(e['Token'])
+            if not challenge:
+                continue
+
+            challenge['standings'] = get_standing_at_round(e, challenge['player_map'])
---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+from log_parser import update_player_map, parse_started_event, update_standings
+
+
+def main(path, out_path, filters):
+    with open(path) as file:
+        log = file.readlines()
+
+    challenges = parse_started_event(log)
+    update_player_map(log, challenges)
+    update_standings(log, challenges)
+
+    for token, challenge in challenges.items():
+        if not filters or any(f in challenge['name'] for f in filters):
+            print(challenge['name'])
+            with open(os.path.join(out_path, challenge['name'] + '.csv'), 'w') as file:
+                df = challenge['standings']
+                for row in [df, *zip(*df.values())]:
+                    file.write(','.join(map(str, row)) + '\n')
+
+
+def get_mtgo_log(mtgo_path):
+    # return last modified log
+    logs = []
+    for dirpath, dirnames, filenames in os.walk(mtgo_path):
+        for file in filenames:
+            if file == 'mtgo.log':
+                logs.append(os.path.join(dirpath, file))
+
+    return max(logs, key=lambda f: os.stat(f).st_mtime)
+
+
+if __name__ == '__main__':
+    mtgo_path = os.path.expanduser('~') + r'\AppData\Local\Apps\2.0'
+    path_to_log = get_mtgo_log(mtgo_path)
+    out_folder = 'data'
+    filters = ['Vintage']
+    os.makedirs(out_folder, exist_ok=True)
+    main(path_to_log, out_folder, filters)
